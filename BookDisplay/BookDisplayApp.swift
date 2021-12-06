8cee2afbd4df7284dc12c2fdfca43167cd1b4aed
BokaiWang <bk.wang1989@gmail.com>
2021-12-06 17:06:00 -0500

    Initial Commit

---
+++ BookDisplay/BookDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookDisplayApp.swift
+//  BookDisplay
+//
+//  Created by 王柏凱 on 2021-12-06.
+//
+
+import SwiftUI
+
+@main
+struct BookDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
